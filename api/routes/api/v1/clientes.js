28324ce17feeeafffc7ab400b56e3981f94512cd
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-12-10 22:28:42 -0300

    router clientes

---
+++ api/routes/api/v1/clientes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+const ClienteController = require('../../../controller/ClienteController')
+const { LojaValidation } = require('../../../controller/validacoes/lojaValidation');
+const { ClienteValidation } = require('../../../controller/validacoes/clienteValidation');
+
+const Validation = require('express-validation');
+const auth = require('../../auth');
+
+const clienteController = new ClienteController();
+
+// Admin
+router.get('/', auth.required, LojaValidation.admin, clienteController.index);
+//router.get('/search/:search/pedidos', auth.required, LojaValidation.admin, clienteController.searchPedidos);
+router.get('/search/:search', auth.required, LojaValidation.admin, clienteController.search);
+
+router.get('/admin/:id', auth.required, LojaValidation.admin, clienteController.showAdmin);
+//router.get('/admin/:id/pedidos', auth.required, LojaValidation.admin, clienteController.showPedidosCliente);
+
+router.put('/admin/:id', auth.required, LojaValidation.admin, clienteController.updateAdmin);
+
+
+// Cliente
+router.get('/:id', auth.required, clienteController.show);
+
+router.post('/', clienteController.store);
+router.put('/:id', auth.required, clienteController.update);
+router.delete('/:id', auth.required, clienteController.remove);
+
+module.exports = router;
--- api/routes/api/v1/index.js
+++ api/routes/api/v1/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/usuarios', require('./usuarios'));
+router.use('/clientes', require('./clientes'));
 router.use('/lojas', require('./lojas'));
 
 module.exports = router;
