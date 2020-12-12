b2eab2dfae2f1a08c9c4d1b139def7eb5ca6b983
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-12-12 17:40:25 -0300

    create ClienteCOntroller

---
+++ api/controller/ClienteController.js
@@ -0,0 +1,153 @@
+const mongoose = require('mongoose');
+
+const Cliente = mongoose.model('Cliente');
+const Usuario = mongoose.model('Usuario');
+
+/**
+ * Admin
+ */
+
+class ClienteController{
+  // GET /index
+  async index(req, res, next) {
+    try{
+      const offset = Number(req.query.offset) || 0;
+      const limit = Number(req.query.limit) || 30;
+      const clientes = await Cliente.paginate(
+        { loja: req.query.loja },
+        { offset, limit, populate: "usuario" }
+      );
+      return res.send({ clientes });
+    } catch(e) {
+      next(e)
+    }
+  }
+
+  // GET /search/:search/pedidos
+  searchPedidos(req, res, next) {
+    return res.status(400).send({ error: "Em desenvolvimento." });
+  }
+
+  // GET /search/:search
+  async search(req, res, next) {
+    const offset = Number(req.query.offset) || 0;
+    const limit = Number(req.query.limit) || 30;
+    const search = new RegExp(req.params.search, "i")
+    try{
+      const clientes = await Cliente.paginate(
+        { loja: req.query.loja, nome: { $regex: search } },
+        { offset, limit, populate: "usuario" }
+      );
+      return res.send({ clientes });
+    } catch(e) {
+      next(e)
+    }
+  }
+
+  // GET /admin/:id
+  async showAdmin(req, res, next) {
+    try {
+      const cliente = await (await Cliente.findOne({ _id: req.params.id, loja: req.query.loja })).populate("usuario");
+      return res.send({ cliente });
+    } catch(e) {
+      next(e)
+    }
+  }
+
+  // GET /admin/:id/pedidos
+  showPedidosCliente(req, res, next) {
+    return res.status(400).send({ error: "Em desenvolvimento." });
+  }
+
+  // PUT /admin/:id
+  async updateAdmin(req, res, next) {
+    const { nome, cpf, email, telefones, endereco, dataDeNascimento } = req.body;
+
+    try{
+      const cliente = await Cliente.findById(req.params.id).populate('usuario');
+      if(nome) {
+        cliente.usuario.nome = nome;
+        cliente.nome = nome;
+      }
+      if(email) cliente.usuario.email = email;
+      if(cpf) cliente.cpf = cpf;
+      if(telefones) cliente.telefones = telefones;
+      if(endereco) cliente.endereco = endereco;
+      if(dataDeNascimento) cliente.dataDeNascimento = dataDeNascimento;
+      await cliente.save();
+      return res.send({ cliente });
+    }catch(e) {
+      next(e)
+    }
+  }
+
+ /**
+ * Cliente
+ */
+
+ // GET /:id
+ async show(req, res, next) {
+   try{
+    const clientes = await Cliente.findOne({ usuario: req.payload.id, loja: req.query.loja }).populate('usuario');
+    return res.send({ clientes });
+   } catch(e) {
+     next(e)
+   }
+ }
+
+ // POST /
+ async store(req, res, next) {
+   const { nome, email, cpf, telefones, endereco, dataDeNascimento, password } = req.body;
+   const { loja } = req.query;
+
+   const usuario = new Usuario({ nome, email, loja });
+   usuario.setSenha(password);
+   const cliente = new Cliente({ nome, cpf, telefones, endereco, loja, dataDeNascimento, usuario: usuario._id });
+
+   try {
+    await usuario.save();
+    await cliente.save();
+
+    return res.send({ cliente: Object.assign({}, cliente._doc, { email: usuario.email }) });
+   } catch(e) {
+     next(e)
+   }
+ }
+
+ async update(req, res, next) {
+  const { nome, email, cpf, telefones, endereco, dataDeNascimento, password } = req.body;
+  try{
+    const cliente = await Cliente.findById(req.payload.id).populate('usuario');
+
+    if(nome) {
+      cliente.usuario.nome = nome;
+      cliente.nome = nome;
+    }
+    if(email) cliente.usuario.email = email;
+    if(password) cliente.usuario.setSenha(password);
+    if(cpf) cliente.cpf = cpf;
+    if(telefones) cliente.telefones = telefones;
+    if(endereco) cliente.endereco = endereco;
+    if(dataDeNascimento) cliente.dataDeNascimento = dataDeNascimento;
+
+    await cliente.save();
+    return res.send({ cliente });
+  } catch(e){
+    next(e)
+  }
+ }
+
+ async remove(req, res, next) {
+   try{
+    const cliente = await Cliente.findOne({ usuario: req.payload.id }).populate('usuario');
+    await cliente.usuario.remove();
+    cliente.deletado = true;
+    await cliente.save();
+    return res.send({ deletado: true })
+   } catch(e) {
+
+   }
+ }
+}
+
+module.exports = ClienteController;
--- api/routes/api/v1/clientes.js
+++ api/routes/api/v1/clientes.js
@@ -10,21 +10,21 @@ const auth = require('../../auth');
 const clienteController = new ClienteController();
 
 // Admin
-router.get('/', auth.required, LojaValidation.admin, clienteController.index);
+router.get('/', auth.required, LojaValidation.admin,Validation(ClienteValidation.index) ,clienteController.index);
 //router.get('/search/:search/pedidos', auth.required, LojaValidation.admin, clienteController.searchPedidos);
-router.get('/search/:search', auth.required, LojaValidation.admin, clienteController.search);
+router.get('/search/:search', auth.required, LojaValidation.admin,Validation(ClienteValidation.search) , clienteController.search);
 
-router.get('/admin/:id', auth.required, LojaValidation.admin, clienteController.showAdmin);
+router.get('/admin/:id', auth.required, LojaValidation.admin,Validation(ClienteValidation.showAdmin) , clienteController.showAdmin);
 //router.get('/admin/:id/pedidos', auth.required, LojaValidation.admin, clienteController.showPedidosCliente);
 
-router.put('/admin/:id', auth.required, LojaValidation.admin, clienteController.updateAdmin);
+router.put('/admin/:id', auth.required, LojaValidation.admin,Validation(ClienteValidation.updateAdmin) , clienteController.updateAdmin);
 
 
 // Cliente
-router.get('/:id', auth.required, clienteController.show);
+router.get('/:id', auth.required,Validation(ClienteValidation.show) , clienteController.show);
 
-router.post('/', clienteController.store);
-router.put('/:id', auth.required, clienteController.update);
+router.post('/',Validation(ClienteValidation.store) , clienteController.store);
+router.put('/:id', auth.required, Validation(ClienteValidation.update) , clienteController.update);
 router.delete('/:id', auth.required, clienteController.remove);
 
 module.exports = router;
