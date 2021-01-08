8dfb2c545ac5b2c583c47d597fd3f646d98fc951
marcoavfcc01121979 <marcoavfcc@gmail.com>
2021-01-07 21:55:19 -0300

    feito a rota de pedidos

---
+++ dashboard/src/app/containers/Pedidos/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Titulo from '../../components/Texto/Titulo';
+
+const Pedidos = () => {
+  return(
+    <div className="Pedidos">
+      <Titulo tipo="h1" titulo="Pedidos" />
+    </div>
+  )
+}
+
+export default Pedidos;
--- dashboard/src/app/index.js
+++ dashboard/src/app/index.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Pedidos from './containers/Pedidos';
+
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        Dashboard
-      </div>
+      <Router>
+        <div className="App">
+          <Route path={"/"} exact component={Pedidos} />
+        </div>
+      </Router>
     </Provider>
   );
 }
