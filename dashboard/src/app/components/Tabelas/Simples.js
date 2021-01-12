0a78fcae78aef4768070a0dcf9076a2e7d94a293
marcoavfcc01121979 <marcoavfcc@gmail.com>
2021-01-12 16:46:10 -0300

    finally Dashbord order

---
+++ dashboard/src/app/components/Tabelas/Simples.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const TabelaSimples = ({ cabecalho, dados }) => (
+  <div className="TabelaSimples">
+    <table className="simples">
+      <thead>
+        <tr>
+          { cabecalho.map((item, idx) => (<th key={idx}>{item}</th>)) }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          dados.map((linha, idx) => (
+            <tr key={idx}>
+              {
+                cabecalho.map((item, index) => (
+                  <td key={index}>{linha[item] || ""}</td>
+                ))
+              }
+              {
+                linha["botaoDetalhes"] && (
+                  <td>
+                    <Link to={linha["botaoDetalhes"]}>
+                      <button>
+                        DETALHES
+                      </button>
+                    </Link>
+                  </td>
+                )
+              }
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  </div>
+)
+
+export default TabelaSimples;
--- dashboard/src/app/containers/Pedidos/index.js
+++ dashboard/src/app/containers/Pedidos/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 import Titulo from '../../components/Texto/Titulo';
 
 import Pesquisa from '../../components/Inputs/Pesquisa';
+import Tabela from '../../components/Tabelas/Simples';
+import Paginacao from '../../components/Paginacao/Simples';
 
 class Pedidos extends Component {
 
@@ -11,8 +14,37 @@ class Pedidos extends Component {
   }
 
   onChangePesquisa = (ev) => this.setState({ pesquisa: ev.target.value });
+
+  changeNumeroAtual = (atual) => this.setState({ atual })
+
   render() {
     const { pesquisa } = this.state;
+
+    // DADOS
+    const dados = [
+      {
+        "Cliente": "Cliente 1",
+        "Valor Total": 89.90,
+        "Data": moment().toISOString(),
+        "Situação": "Aguardando pagamento",
+        "botaoDetalhes": "/pedido/hdfhfdjdfkfdklafhjlkh"
+      },
+      {
+        "Cliente": "Cliente 2",
+        "Valor Total": 109.92,
+        "Data": moment().toISOString(),
+        "Situação": "Aguardando pagamento",
+        "botaoDetalhes": "/pedido/54+89563636"
+      },
+      {
+        "Cliente": "Cliente 3",
+        "Valor Total": 39.53,
+        "Data": moment().toISOString(),
+        "Situação": "Pagamento concluido",
+        "botaoDetalhes": "/pedido/54+984213hklafhjlkh"
+      },
+    ]
+
   return(
       <div className="Pedidos">
         <div className="Card">
@@ -22,10 +54,19 @@ class Pedidos extends Component {
             valor={pesquisa}
             placeholder={"Pesquisa aqui pelo nome do cliente..."}
             onChange={(ev) => this.onChangePesquisa(ev)}
+            onClick={() => alert("Pesquisar")}
           />
           <br />
-          <Tabela />
-          <Paginacao />
+          <Tabela 
+            cabecalho={["Cliente", "Valor Total", "Data", "Situação"]}
+            dados={dados}
+          />
+        
+          <Paginacao 
+            atual={this.state.atual} 
+            total={120} 
+            limite={20} 
+            onClick={(numeroAtual) => this.changeNumeroAtual(numeroAtual)} />
         </div>
       </div>
     )
